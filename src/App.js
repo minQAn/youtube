9f0efd2fd713dad5ffe43ff981ac7bdfa94689ee
minQ <minq.an7@gmail.com>
2022-07-16 15:07:30 -0400

    show video list with title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [videos, setVideos] = useState([]);
+  const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${YOUTUBE_API_KEY}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const VideoItem = (props) => <div>{props.video.snippet.title}</div>;
+
+export default VideoItem;
